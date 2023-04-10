767616e6b5b6c5b5f4ff4dffd2d45c2fac70cb80
imransaleemofficial <imransaleem987789@gmail.com>
2023-04-10 15:54:23 +0500

    2nd Change on 10th April 2023

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
@@ -10,7 +9,8 @@ import IdeaxNavbar from "./components/IdeaxNavbar/IdeaxNavbar";
 import BuyerForm from "./components/BuyerForm/BuyerForm.js"
 import FooterSection from "./components/FooterSection/footersection";
 import SellerSide from "./components/SellerSide/SellerSide";
-
+import PrivacyPolicy from "./components/IdeaxNavbar/PrivacyPolicy/PrivacyPolicy";
+import UseOfCookies from "./components/FooterSection/UseOfCookies/UseOfCookies";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -19,7 +19,6 @@ import WebFont from 'webfontloader';
 import { auth } from "./firebase";
 
 import "./App.css";
-import PrivacyPolicy from "./components/IdeaxNavbar/PrivacyPolicy/PrivacyPolicy";
 
 function App() {
   const [userName, setUserName] = useState("");
@@ -34,7 +33,8 @@ function App() {
   useEffect(() => {
     WebFont.load({
       google: {
-        families: ['Droid Sans', 'Chilanka','Droid Serif',"Oswald", "Roboto", "Inter", "Alkatra","Fasthand","Nunito" ]
+        families: ['Droid Sans', 'Chilanka','Droid Serif',"Oswald", 
+        "Roboto", "Inter", "Alkatra","Fasthand","Nunito" ]
       }
     });
    }, []);
@@ -54,6 +54,7 @@ function App() {
           <Route path="/" element={<Home name={userName} />} />
           <Route path="/entrepreneurs" element={<SellerSide />} />
           <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+          <Route path="/useofcookies" element={<UseOfCookies />} />
         </Routes>
       </Router>
 
---
+++ src/components/FooterSection/UseOfCookies/UseOfCookies.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import '../UseOfCookies/UseOfCookies.css';
+
+const UseOfCookies = () => {
+  return (
+    <div>
+        <h4 className='heading_useofcookies'>COOKIE POLICY IDEAX.pk </h4>
+        <h1 className='heading_useofcookies'>The use of cookies on our website</h1>
+        <p className='para_useofcookies'>When you visit our website, we place cookies on your computer or mobile device. These cookies are used to optimise your user experience. </p>
+        <h2 className='heading_useofcookies'>What are cookies? </h2>
+        <p className='para_useofcookies'>“Cookies” are small files or pieces of information sent out by this website. These cookie files can
+                be stored on your computer or mobile device when you visit the website. They record
+                    information about the type of browser you use, your operating system and your geographical
+                        location. 
+        </p>
+        <h2 className='heading_useofcookies'>How we use cookies? </h2>
+        <p className='para_useofcookies'>We use cookies to make it easier for you to use our website and to distinguish your user
+                preferences from those of other visitors to our site. Without cookies, a website is like a goldfish
+                that loses its memory every time you visit a new page. By placing cookies on your device, our
+                website remembers your language preference, for example. 
+        </p>
+        <p className='para_useofcookies'>We also use cookies to compile anonymous, aggregate statistics, so that we have a clear idea of
+                how visitors use our website. We use this information to optimise the user experience. 
+        </p>
+        <p className='para_useofcookies'>The information we collect is used only within our organisation. We do not pass your data along
+                to third parties. 
+        </p>  
+        <h3 className='heading_useofcookies'>What are the different types of cookies?</h3>
+        <p className='para_useofcookies'>There are various categories of cookies: </p>
+        <h4 className='heading_useofcookies'>Essential cookies</h4>
+        <p className='para_useofcookies'>These cookies are required in order for the website to function. Since these cookies are essential
+                for the website's functionality, you cannot refuse these cookies without this having an impact on
+                the functioning of our website. You can remove these cookies, though. For more information
+                about this, see ‘How do I manage my cookies?’. 
+        </p>
+        <h4 className='heading_useofcookies'>Functional cookies</h4>
+        <p className='para_useofcookies'>These cookies collect information about your preferences. For example, they record your
+                language preference or log-in data.     
+        </p>
+        <h4 className='heading_useofcookies'>Analytical cookies</h4>
+        <p className='para_useofcookies'>These types of cookies collect information about the behaviour of website visitors and the
+                performance of the website. For example, data about how long someone stays on the website is
+                recorded. This allows us to improve the user experience of the website. This information is
+                collected completely anonymously.
+        </p>
+        <h4 className='heading_useofcookies'>Marketing cookies</h4>
+        <p className='para_useofcookies'>Marketing cookies are used to monitor the surfing behaviour of visitors. Based on this
+                information, a user profile is created which allows more personalised information to be sent out
+                to you. This website does not use marketing cookies. 
+        </p>
+        <h4 className='heading_useofcookies'>How can I manage my cookies? </h4>
+        <p className='para_useofcookies'>You always have the right to accept or decline certain cookies. 
+        </p>
+        <p className='para_useofcookies'>When you first visit our website, you are asked to accept our various types of cookies. 
+        </p>
+        <p className='para_useofcookies'>You may also adjust your browser settings so that it accepts or declines these cookies. If you
+                choose to decline all cookies, then you can still use our website, but we cannot guarantee that
+                all of the website's functionality will work or that you will have access to all parts of our website.
+                Since the procedure for declining cookies varies from browser to browser, it is best if you
+                consult your browser menu. 
+        </p>
+
+    </div>
+  )
+}
+
+export default UseOfCookies
